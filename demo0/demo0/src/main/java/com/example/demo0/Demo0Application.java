f8bc526c8fe408d4703858a9e2b2c0e92aa8436e
Tangdaidai <1042021953@qq.com>
2020-11-24 00:40:11 +0800

    the 2 commit

--- demo0/demo0/src/main/java/com/example/demo0/Demo0Application.java
+++ demo0/demo0/src/main/java/com/example/demo0/Demo0Application.java
@@ -1,17 +1,22 @@
 package com.example.demo0;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableConfigurationProperties(TestConfigBean.class)
 public class Demo0Application {
+	@Autowired
+	private TestConfigBean testConfigBean;
 
 	@RequestMapping
 	String index() {
-		return "Hello Spring Boot!";
+		return testConfigBean.getName()+"——"+testConfigBean.getAge();
 	}
 	
 	public static void main(String[] args) {
